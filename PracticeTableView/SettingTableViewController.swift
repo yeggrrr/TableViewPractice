8150671aeda97e95a346ae48fe7d7c8fe3f21b9d
yeggrrr <yjkim981103@gmail.com>
2024-05-23 17:15:28 +0900

    Initial Commit

---
+++ PracticeTableView/SettingTableViewController.swift
@@ -0,0 +1,74 @@
+//
+//  SettingTableViewController.swift
+//  PracticeTableView
+//
+//  Created by YJ on 5/23/24.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+    @IBOutlet var titleLabel: UILabel!
+    
+    var header = ["전체 설정", "개인 설정", "기타"]
+    
+    var entireSetting = ["공지사항", "실험실", "버전 정보"]
+    var personalSetting = ["개인/보안", "알림", "채팅", "멀티프로필"]
+    var etc = ["고객센터/도움말"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = "설정"
+        titleLabel.font = .boldSystemFont(ofSize: 17)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return header.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return header[section]
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
+        guard let header = view as? UITableViewHeaderFooterView else { return }
+        header.textLabel?.textColor = UIColor.darkGray
+        header.textLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return entireSetting.count
+        } else if section == 1 {
+            return personalSetting.count
+        } else {
+            return etc.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell")!
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = entireSetting[indexPath.row]
+            cell.textLabel?.textColor = .black
+            cell.textLabel?.font = .systemFont(ofSize: 14)
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = personalSetting[indexPath.row]
+            cell.textLabel?.textColor = .black
+            cell.textLabel?.font = .systemFont(ofSize: 14)
+        } else {
+            cell.textLabel?.text = etc[indexPath.row]
+            cell.textLabel?.textColor = .black
+            cell.textLabel?.font = .systemFont(ofSize: 14)
+        }
+        return cell
+    }
+}
---
+++ PracticeTableView/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PracticeTableView
+//
+//  Created by YJ on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+

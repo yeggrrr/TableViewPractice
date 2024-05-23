fb7d24e5fbae1fe98f514a46f8c0585ac7914ecb
yeggrrr <yjkim981103@gmail.com>
2024-05-23 17:28:41 +0900

    [Feat] 스토리보드로 테이블 뷰 만들기(Case1) & 코드로 테이블 뷰 만들기(Case2)

--- PracticeTableView/SettingTableViewController.swift
+++ PracticeTableView/SettingTableViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class SettingTableViewController: UITableViewController {
-    @IBOutlet var titleLabel: UILabel!
     
     var header = ["전체 설정", "개인 설정", "기타"]
     
@@ -19,8 +18,7 @@ class SettingTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = "설정"
-        titleLabel.font = .boldSystemFont(ofSize: 17)
+        title = "설정"
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -34,8 +32,10 @@ class SettingTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         
         guard let header = view as? UITableViewHeaderFooterView else { return }
-        header.textLabel?.textColor = UIColor.darkGray
-        header.textLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        header.textLabel?.textColor = UIColor.lightGray
+        header.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        header.textLabel?.frame = header.bounds
+        header.textLabel?.textAlignment = .left
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,15 +58,15 @@ class SettingTableViewController: UITableViewController {
         
         if indexPath.section == 0 {
             cell.textLabel?.text = entireSetting[indexPath.row]
-            cell.textLabel?.textColor = .black
+            cell.textLabel?.textColor = UIColor.label
             cell.textLabel?.font = .systemFont(ofSize: 14)
         } else if indexPath.section == 1 {
             cell.textLabel?.text = personalSetting[indexPath.row]
-            cell.textLabel?.textColor = .black
+            cell.textLabel?.textColor = UIColor.label
             cell.textLabel?.font = .systemFont(ofSize: 14)
         } else {
             cell.textLabel?.text = etc[indexPath.row]
-            cell.textLabel?.textColor = .black
+            cell.textLabel?.textColor = UIColor.label
             cell.textLabel?.font = .systemFont(ofSize: 14)
         }
         return cell

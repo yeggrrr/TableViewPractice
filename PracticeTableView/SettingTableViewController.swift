ed158966bf3dabcd8a73aa19047e5494079be4f8
yeggrrr <yjkim981103@gmail.com>
2024-06-04 09:23:11 +0900

    [Fix] 테이블뷰컨트롤러 코드 개선

--- PracticeTableView/SettingTableViewController.swift
+++ PracticeTableView/SettingTableViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
+enum Setting: Int, CaseIterable {
+    case entireSetting
+    case personalSetting
+    case etc
+    
+    var mainOption: String {
+        switch self {
+        case.entireSetting:
+            return "전체 설정"
+        case.personalSetting:
+            return "개인 설정"
+        case.etc:
+            return "기타"
+            
+        }
+    }
+    
+    var optionString: [String] {
+        switch self {
+        case.entireSetting:
+            return ["공지사항", "실험실", "버전 정보"]
+        case.personalSetting:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case.etc:
+            return ["고객센터/도움말"]
+        }
+    }
+}
+
 class SettingTableViewController: UITableViewController {
-    
-    var header = ["전체 설정", "개인 설정", "기타"]
-    
-    var entireSetting = ["공지사항", "실험실", "버전 정보"]
-    var personalSetting = ["개인/보안", "알림", "채팅", "멀티프로필"]
-    var etc = ["고객센터/도움말"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +45,12 @@ class SettingTableViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return header.count
+        return Setting.allCases.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return header[section]
+        return Setting.allCases[section].mainOption
+        
     }
     
     override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
@@ -39,13 +63,7 @@ class SettingTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return entireSetting.count
-        } else if section == 1 {
-            return personalSetting.count
-        } else {
-            return etc.count
-        }
+        return Setting.allCases[section].optionString.count
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -55,20 +73,10 @@ class SettingTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell")!
-        
-        if indexPath.section == 0 {
-            cell.textLabel?.text = entireSetting[indexPath.row]
-            cell.textLabel?.textColor = UIColor.label
-            cell.textLabel?.font = .systemFont(ofSize: 14)
-        } else if indexPath.section == 1 {
-            cell.textLabel?.text = personalSetting[indexPath.row]
-            cell.textLabel?.textColor = UIColor.label
-            cell.textLabel?.font = .systemFont(ofSize: 14)
-        } else {
-            cell.textLabel?.text = etc[indexPath.row]
-            cell.textLabel?.textColor = UIColor.label
-            cell.textLabel?.font = .systemFont(ofSize: 14)
-        }
+
+        cell.textLabel?.text = Setting.allCases[indexPath.section].optionString[indexPath.row]
+        cell.textLabel?.textColor = UIColor.label
+        cell.textLabel?.font = .systemFont(ofSize: 14)
         return cell
     }
 }
